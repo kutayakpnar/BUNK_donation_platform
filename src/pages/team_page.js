6111d415341b39066bf47e2efdba7901723d884f
Kutay Akpınar <kutayakpnar@gmail.com>
2024-06-01 03:21:37 +0300

    team page + contact page modification

--- src/pages/team_page.js
+++ src/pages/team_page.js
@@ -1,16 +1,23 @@
 import MemberCard from "../components/member_card";
 import "../styles/team_page.css";
 import default_member_image from "../assets/profile-user.png";
-
+import basri from "../assets/basri.jpeg";
+import ünal from "../assets/ünal.jpeg"
+import niyazi from "../assets/niyazi.jpeg"
+import kutay from "../assets/kutay.jpg"
 const TeamPage = () => {
     return (
-    <div className="member_card_cover">
-        <h2>Meet with BUNK Team!</h2>
-        <MemberCard member_image_url={default_member_image} member_name="Ünal Dalkılıç" member_info="Ünal Dalkılıç info will goes here"/>
-        <MemberCard member_image_url={default_member_image} member_name="Mehmet Kutay Akpınar" member_info="Kutay Akpınar info will goes here"/>
-        <MemberCard member_image_url={default_member_image} member_name="Hasan Basri Erdoğan" member_info="Hasan Basri Erdoğan info will goes here"/>
-        <MemberCard member_image_url={default_member_image} member_name="Niyazi Alperen Tuğan" member_info="Niyazi Alperen Tuğan info will goes here"/>
-    </div>
+        <div className="member_card_cover">
+            <h2>Meet with BUNK Team!</h2>
+            <MemberCard member_image_url={ünal} member_name="Ünal Dalkılıç" member_info="Unal DALKILIC is a third-year Computer Engineering student at İzmir Institute of High Technology. He is responsible for frontend development on the BUNK donation platform." />
+
+            <MemberCard member_image_url={kutay} member_name="Mehmet Kutay Akpınar" member_info="Mehmet Kutay AKPINAR is a fourth-year Computer Engineering student at İzmir Institute of High Technology. He is responsible for frontend development on the BUNK donation platform." />
+
+            <MemberCard member_image_url={basri} member_name="Hasan Basri Erdoğan" member_info="Hasan Basri ERDOGAN is a third-year Computer Engineering student at İzmir Institute of High Technology. He is involved in backend development for the BUNK donation platform." />
+            
+            <MemberCard member_image_url={niyazi} member_name="Niyazi Alperen Tuğan" member_info="Niyazi Alperen TUGAN is a fourth-year Computer Engineering student at İzmir Institute of High Technology. He is involved in backend development for the BUNK donation platform." />
+
+        </div>
     );
 };
 
