400f02a590d8f9f8677625986485ce39915863a5
Ünal Dalkılıç <unal3502@gmail.com>
2024-06-03 10:54:44 +0300

    responsive design added to all pages

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,19 @@
+import { setState, useState } from "react";
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
 import "../styles/header.css";
 
 const Header = () => {
+    const [responsive_button, setResponsiveButton] = useState(true);
+    
     return (
         <header>
             <div className="logo">
                 <Link to="/"><img src={logo} alt="WFP Logo" className="logo-image" /></Link>
                 <h1 className="site-title">BUNK Donation Platform</h1>
+                <div className="responsive_button background_contain" onClick={() => {setResponsiveButton(!responsive_button)}}></div>
             </div>
-            <nav>
+            <nav style={{display: (responsive_button) ? "block" : "none"}}>
                 <ul>
                     <Link to="/who-we-are"><li>Our Team</li></Link>
                     <Link to="/our-work"><li>Stock</li></Link>
